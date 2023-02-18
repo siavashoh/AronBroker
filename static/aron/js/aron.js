c0fd2d86286241ed8b5c7064989d3f3ba191c470
ghasemi-zeinab <me.zeinabghasemi@gmail.com>
2023-02-18 12:20:52 +0330

    changes

--- static/aron/js/aron.js
+++ static/aron/js/aron.js
@@ -46,6 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
 $(function () {
   var s1 = $("#slider").sliderRotate({ displayItems: 5 });
   var s2 = $("#slider-2").sliderRotate({ autoSlide: true });
+  var s2 = $("#slider-3").sliderRotate({ autoSlide: true });
 });
 
 (function ($, window, document, undefined) {
@@ -73,6 +74,11 @@ $(function () {
     this.NEXT_CLASS = "slider-rotate__item--next";
     this.NEXT2_CLASS = "slider-rotate__item--next-2";
     this.ACTIVE_CLASS = "slider-rotate__item--active";
+    this.PREV_CLASS2 = "slider-rotate__item_2--prev";
+    this.PREV2_CLASS2 = "slider-rotate__item_2--prev-2";
+    this.NEXT_CLASS2 = "slider-rotate__item_2--next";
+    this.NEXT2_CLASS2 = "slider-rotate__item_2--next-2";
+    this.ACTIVE_CLASS2 = "slider-rotate__item_2--active";
 
     this.CLASS_DISPLAY_3 = "slider-rotate--3";
     this.CLASS_DISPLAY_5 = "slider-rotate--5";
@@ -146,6 +152,7 @@ $(function () {
     __this__.$item.on("click.rotate", function () {
       //prevent click on active item
       if ($(this).hasClass(__this__.ACTIVE_CLASS)) return false;
+      if ($(this).hasClass(__this__.ACTIVE_CLASS2)) return false;
 
       //move slide to the desired index
       _moveSlide($(this).index(), __this__);
@@ -241,22 +248,43 @@ $(function () {
     __this__.plugin_element
       .find("." + __this__.NEXT2_CLASS)
       .removeClass(__this__.NEXT2_CLASS);
+    __this__.plugin_element
+      .find("." + __this__.ACTIVE_CLASS2)
+      .removeClass(__this__.ACTIVE_CLASS2);
+    __this__.plugin_element
+      .find("." + __this__.NEXT_CLASS2)
+      .removeClass(__this__.NEXT_CLASS2);
+    __this__.plugin_element
+      .find("." + __this__.PREV_CLASS2)
+      .removeClass(__this__.PREV_CLASS2);
+    __this__.plugin_element
+      .find("." + __this__.PREV2_CLASS2)
+      .removeClass(__this__.PREV2_CLASS2);
+    __this__.plugin_element
+      .find("." + __this__.NEXT2_CLASS2)
+      .removeClass(__this__.NEXT2_CLASS2);
 
     //if active index is the last item
     if (__index__ == __this__.numItens - 1) {
       __this__.$item.eq(0).addClass(__this__.NEXT_CLASS);
+      __this__.$item.eq(0).addClass(__this__.NEXT_CLASS2);
 
       if (__this__.displayItens == __this__.DISPLAY_5) {
         __this__.$item.eq(1).addClass(__this__.NEXT2_CLASS);
+        __this__.$item.eq(1).addClass(__this__.NEXT2_CLASS2);
       }
     }
 
     //if active index is the first item
     if (__index__ == 0) {
       __this__.$item.eq(__this__.numItens - 1).addClass(__this__.PREV_CLASS);
+      __this__.$item.eq(__this__.numItens - 1).addClass(__this__.PREV_CLASS2);
 
       if (__this__.displayItens == __this__.DISPLAY_5) {
         __this__.$item.eq(__this__.numItens - 2).addClass(__this__.PREV2_CLASS);
+        __this__.$item
+          .eq(__this__.numItens - 2)
+          .addClass(__this__.PREV2_CLASS2);
       }
     }
 
@@ -264,34 +292,41 @@ $(function () {
     __this__.$item.each(function (index) {
       if (index == __index__) {
         __this__.$item.eq(index).addClass(__this__.ACTIVE_CLASS);
+        __this__.$item.eq(index).addClass(__this__.ACTIVE_CLASS2);
       }
 
       if (index == __index__ + 1) {
         __this__.$item.eq(index).addClass(__this__.NEXT_CLASS);
+        __this__.$item.eq(index).addClass(__this__.NEXT_CLASS2);
       }
 
       if (index == __index__ - 1) {
         __this__.$item.eq(index).addClass(__this__.PREV_CLASS);
+        __this__.$item.eq(index).addClass(__this__.PREV_CLASS2);
       }
 
       //just addClass if display 5 items
       if (__this__.displayItens == __this__.DISPLAY_5) {
         if (index == __index__ + 2) {
           __this__.$item.eq(index).addClass(__this__.NEXT2_CLASS);
+          __this__.$item.eq(index).addClass(__this__.NEXT2_CLASS2);
         }
 
         if (__index__ == __this__.numItens - 2) {
           __this__.$item.eq(0).addClass(__this__.NEXT2_CLASS);
+          __this__.$item.eq(0).addClass(__this__.NEXT2_CLASS2);
         }
 
         if (__index__ - 2 == -1) {
           __this__.$item
             .eq(__this__.numItens - 1)
-            .addClass(__this__.PREV2_CLASS);
+            .addClass(__this__.PREV2_CLASS)
+            .addClass(__this__.PREV2_CLASS2);
         }
 
         if (index == __index__ - 2) {
           __this__.$item.eq(index).addClass(__this__.PREV2_CLASS);
+          __this__.$item.eq(index).addClass(__this__.PREV2_CLASS2);
         }
       }
 
@@ -319,8 +354,6 @@ $(function () {
 
 /////////////////////////////////////////////// market /////////////////////////////////////////////////
 for (var d in data) {
-  // const ctx = document.getElementById(data[d].currency.base);
-
   var date_labels = [];
   var price_data = [];
 
@@ -330,7 +363,6 @@ for (var d in data) {
   }
 
   let borderColor = "rgb(50,205,50)";
-  let backgroundColor = "rgb(220, 255, 220)";
   var canvas = document.getElementById(data[d].currency.base);
   var ctx = canvas.getContext("2d");
   var gradient = ctx.createLinearGradient(0, 0, 0, 80);
@@ -396,7 +428,7 @@ var $owl = $("#s-list").owlCarousel({
       items: 2,
     },
     1000: {
-      items: 4,
+      items: 3,
     },
   },
 });
@@ -420,9 +452,87 @@ var $owl2 = $("#options").owlCarousel({
   },
 });
 //////////////////////////////////////////// Market overview //////////////////////////////////////
+for (var md in data) {
+  var date_labels = [];
+  var price_data = [];
+
+  for (var i in data[md].data) {
+    date_labels.push(data[md].data[i].time_period_start);
+    price_data.push(data[md].data[i].rate_close);
+  }
+
+  let borderColor = "rgb(50,205,50)";
+  var canvas2 = document.getElementById(`${data[md].currency.base}2`);
+  var ctx2 = canvas2.getContext("2d");
+  var gradient = ctx2.createLinearGradient(0, 0, 0, 80);
+  gradient.addColorStop(0, "rgb(220, 255, 220)");
+  gradient.addColorStop(1, "rgba(255,255,255,1)");
+
+  if (data[md].currency.last_24h < 0) {
+    borderColor = "rgb(255, 50, 50)";
+    gradient = ctx.createLinearGradient(0, 0, 0, 80);
+    gradient.addColorStop(0, "rgb(255, 220, 220)");
+    gradient.addColorStop(1, "rgba(255,255,255,1)");
+  }
+
+  new Chart(ctx2, {
+    type: "line",
+    data: {
+      labels: date_labels /* date */,
+      responsive: true,
+      datasets: [
+        {
+          borderColor: borderColor,
+          backgroundColor: gradient,
+          fill: true,
+          data: price_data /* price */,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      events: [],
+      elements: {
+        point: {
+          radius: 0,
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        x: {
+          display: false,
+        },
+        y: {
+          display: false,
+        },
+      },
+    },
+  });
+}
 var $owl3 = $("#market-overview").owlCarousel({
   loop: false,
   margin: 5,
+  nav: false,
+  stagePadding: 50,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+var $owl3 = $("#market-overview-sm").owlCarousel({
+  loop: false,
+  margin: 0,
   nav: false,
   stagePadding: 50,
   responsive: {
